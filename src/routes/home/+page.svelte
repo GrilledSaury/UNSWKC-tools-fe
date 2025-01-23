4b6a471aed78f806be87b82a701971a9b0470b8a
VampireRabbit <1662025808@qq.com>
2025-01-22 21:36:43 -0500

    update home

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -16,6 +16,19 @@
     .catch(err => {
       console.log(err)
     })
+
+  async function toProfile () {
+    goto('/profile')
+  }
+
+  async function toBeginnerCourse () {
+    goto('/beginner')
+  }
+
 </script>
 
-<p>Welcome, {user.name}</p>
+<div class="h-screen w-screen bg-gray-100 flex flex-col p-3 sm:p-10">
+  <h1 class="text-gray-700 font-bold text-3xl m-3">Welcome, {user.name}</h1>
+  <button class="m-2 px-3 py-2 rounded shadow bg-white flex w-3/4" onclick={toProfile}>profile</button>
+  <button class="m-2 px-3 py-2 rounded shadow bg-white flex w-3/4" onclick={toBeginnerCourse}>beginner course</button>
+</div>
---
+++ src/routes/home/profile/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { getAuth } from "firebase/auth";
+  import { collection, getDocs } from "firebase/firestore";
+  import { db } from "../../lib/firebase";
+</script>
+
+<p>profile</p>
