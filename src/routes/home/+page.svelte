6114ad6453bef91689e6f1877e28c3c2a11daa52
Matthew Qiao <33226552+GrilledSaury@users.noreply.github.com>
2025-01-23 14:36:35 +1100

    Update +page.svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -35,12 +35,12 @@
 <div class="h-screen w-screen bg-gray-100 flex flex-col p-3 sm:p-10">
   <h1 class="text-gray-700 font-bold text-3xl m-3">Welcome, {user.name}</h1>
   <div class="flex flex-col sm:flex-row">
-    <button class="m-2 px-3 py-2 rounded shadow bg-white flex transition-all hover:bg-blue-100 flex items-center justify-center" onclick={toProfile}>
-      <Icon icon={UserCircle} class="text-2xl text-blue-500 mr-1"/>
+    <button class="m-2 px-3 py-2 rounded shadow bg-white transition-all hover:bg-blue-100 flex items-center sm:justify-center" onclick={toProfile}>
+      <Icon icon={UserCircle} class="text-2xl text-blue-500 mr-3 sm:mr-1"/>
       My Profile
     </button>
-    <button class="m-2 px-3 py-2 rounded shadow bg-white flex transition-all hover:bg-blue-100 flex items-center justify-center" onclick={toBeginnerCourse}>
-      <Icon icon={AcademicCap} class="text-2xl text-yellow-500 mr-1"/>
+    <button class="m-2 px-3 py-2 rounded shadow bg-white transition-all hover:bg-blue-100 flex items-center sm:justify-center" onclick={toBeginnerCourse}>
+      <Icon icon={AcademicCap} class="text-2xl text-yellow-500 mr-3 sm:mr-1"/>
       Beginner Course
     </button>
   </div>
