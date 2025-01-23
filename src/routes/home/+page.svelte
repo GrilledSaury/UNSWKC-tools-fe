503d54ec4e92e728eeeb9ee9e1577aca9283fc94
Matthew Qiao <33226552+GrilledSaury@users.noreply.github.com>
2025-01-23 14:34:15 +1100

    feat: icons for navigation buttons in home page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,14 +1,17 @@
 <script>
-  import { getAuth } from "firebase/auth";
-  import { getDoc, doc } from "firebase/firestore";
-  import { db } from "../../lib/firebase";
+  import { getAuth } from "firebase/auth"
+  import { getDoc, doc } from "firebase/firestore"
+  import { db } from "../../lib/firebase"
+  import Icon from "heroicons-for-svelte"
+  import { UserCircle, AcademicCap } from "heroicons-for-svelte/icons/solid"
 
   import { goto } from '$app/navigation'
 
-  const auth = getAuth();
-  const authUser = auth.currentUser;
-  let user = $state({ name: '' })
+  const auth = getAuth()
+  const authUser = auth.currentUser
   if (authUser === null) goto('/')
+  
+  let user = $state({ name: '' })
 
   async function loadUser () {
     const docRef = doc(db, 'user', authUser.uid)
@@ -31,6 +34,14 @@
 
 <div class="h-screen w-screen bg-gray-100 flex flex-col p-3 sm:p-10">
   <h1 class="text-gray-700 font-bold text-3xl m-3">Welcome, {user.name}</h1>
-  <button class="m-2 px-3 py-2 rounded shadow bg-white flex w-3/4" onclick={toProfile}>profile</button>
-  <button class="m-2 px-3 py-2 rounded shadow bg-white flex w-3/4" onclick={toBeginnerCourse}>beginner course</button>
+  <div class="flex flex-col sm:flex-row">
+    <button class="m-2 px-3 py-2 rounded shadow bg-white flex transition-all hover:bg-blue-100 flex items-center justify-center" onclick={toProfile}>
+      <Icon icon={UserCircle} class="text-2xl text-blue-500 mr-1"/>
+      My Profile
+    </button>
+    <button class="m-2 px-3 py-2 rounded shadow bg-white flex transition-all hover:bg-blue-100 flex items-center justify-center" onclick={toBeginnerCourse}>
+      <Icon icon={AcademicCap} class="text-2xl text-yellow-500 mr-1"/>
+      Beginner Course
+    </button>
+  </div>
 </div>
